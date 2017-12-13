bf676944b9e52fafb1b1f3f30146f16aed40471f
Jessica Kerr <jessitron@gmail.com>
2017-12-13 18:41:44 +0000

    Initial commit from Atomist

---
+++ src/main/java/com/wiggly/octopus/OctopusApplication.java
@@ -0,0 +1,14 @@
+package com.wiggly.octopus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@ComponentScan
+@SpringBootApplication
+public class OctopusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctopusApplication.class, args);
+	}
+}
