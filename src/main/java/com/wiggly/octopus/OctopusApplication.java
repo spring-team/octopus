7ea0a5b6ea49a496ac3ea180e2191b41168d45e8
Jessica Kerr <jessitron@gmail.com>
2017-12-13 18:42:25 +0000

    Remove unnecessary component scan annotations

--- src/main/java/com/wiggly/octopus/OctopusApplication.java
+++ src/main/java/com/wiggly/octopus/OctopusApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@ComponentScan
 @SpringBootApplication
 public class OctopusApplication {
 
